00f4080195fbe896f184ff7b86554997af803cf4
zoryzhang <hereiszory@gmail.com>
2024-05-20 01:59:40 -0500

    minor

--- CLI.py
+++ CLI.py
@@ -163,7 +163,7 @@ class OptunaCLI(LightningCLI):
         
         # in case of other operations
         if ckpt_path:
-            self.model = type(self.model).load(ckpt_path, **kwargs)
+            self.model = type(self.model).load(ckpt_path, device=self.model.device, freeze=True, **kwargs)
         return ret
 
 def cli_main(trial: optuna.trial.Trial = None):
